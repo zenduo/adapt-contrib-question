4f1e6916fd245375ea49fb8431b51aaecc712529
Daryl Hedley <darylhedley@hotmail.com>
2014-06-24 09:23:57 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
-adapt-contrib-question
-======================
+#adapt-contrib-gmcq
 
 A skeleton for building question components
+
+##Installation
+
+
+First, be sure to install the [Adapt Command Line Interface](https://github.com/adaptlearning/adapt-cli), then from the command line run:-
+
+        adapt install adapt-contrib-question
+
+This component can also be installed by adding the component to the adapt.json file before running `adapt install`:
+
+        "adapt-contrib-question": "*"
+
+##Usage
+
+To Be Updated
+
+##Settings overview
+ 
+An complete example of this components settings can be found in the [example.json](https://github.com/adaptlearning/adapt-contrib-gmcq/blob/master/example.json) file. A description of the core settings can be found at: [Core model attributes](https://github.com/adaptlearning/adapt_framework/wiki/Core-model-attributes)
+
+Further settings for this component are:
+
+####_component
+
+This value must be: `question`
+
+####_classes
+
+You can use this setting to add custom classes to your template and LESS file.
+
+####_layout
+
+This defines the position of the component in the block. Values can be `full`, `left` or `right`. 
+
+####_attempts
+
+Default: `1`
+
+Specifies the number of attempts for this question.
+
+####_items
+
+##Limitations
+ 
+To be completed.
+
+##Browser spec
+
+This component has been tested to the standard Adapt browser specification.
---
+++ js/adapt-contrib-question.js
@@ -0,0 +1,77 @@
+/*
+* adapt-contrib-question
+* License - http://github.com/adaptlearning/adapt_framework/LICENSE
+* Maintainers - Daryl Hedley <darylhedley@gmail.com>
+*/
+define(function(require) {
+
+    var QuestionView = require('coreViews/questionView');
+    var Adapt = require('coreJS/adapt');
+    
+    var Question = QuestionView.extend({
+
+        events: {},
+
+        // Used by question to disable the question during submit and complete stages
+        disableQuestion: function() {},
+
+        // Used by question to enable the question during interactions
+        enableQuestion: function() {},
+
+        // Used by the question to reset the question when revisiting the component
+        resetQuestionOnRevisit: function() {},
+
+        // Left blank for question setup - should be used instead of preRender
+        setupQuestion: function() {},
+
+        // Blank method used just like postRender is for presentational components
+        onQuestionRendered: function() {},
+
+        // Use to check if the user is allowed to submit the question
+        // Maybe the user has to select an item?
+        // Should return a boolean
+        canSubmit: function() {},
+
+        // Blank method for question to fill out when the question cannot be submitted
+        onCannotSubmit: function() {},
+
+        // This is important for returning or showing the users answer
+        // This should preserve the state of the users answers
+        storeUserAnswer: function() {},
+
+        // Should return a boolean based upon whether to question is correct or not
+        isCorrect: function() {},
+
+        // Used to set the score based upon the _questionWeight
+        setScore: function() {},
+
+        // This is important and should give the user feedback on how they answered the question
+        // Normally done through ticks and crosses by adding classes
+        showMarking: function() {},
+
+        // Used by the question to determine if the question is incorrect or partly correct
+        // Should return a boolean
+        isPartlyCorrect: function() {},
+
+        // Used by the question view to reset the stored user answer
+        resetUserAnswer: function() {},
+
+        // Used by the question view to reset the look and feel of the component.
+        // This could also include resetting item data
+        resetQuestion: function() {},
+
+        // Used by the question to display the correct answer to the user
+        showCorrectAnswer: function() {},
+
+        // Used by the question to display the users answer and
+        // hide the correct answer
+        // Should use the values stored in storeUserAnswer
+        hideCorrectAnswer: function() {}
+
+    });
+    
+    Adapt.register("question", Question);
+
+    return Question;
+    
+});
